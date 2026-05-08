538a16e9a5c42358918b64b224d8d4413f6b542b
KEHAN700 <bogdangas2010@gmail.com>
2026-05-08 22:46:46 +0300

    fix 3.0 route

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -9,11 +9,10 @@ import {
 
 const notesRouter = Router();
 
-notesRouter.get("/notes", getAllNotes);
-notesRouter.get("/notes/:noteId", getNoteById);
-notesRouter.post("/notes", createNote);
-notesRouter.delete("/notes/:noteId", deleteNote);
-notesRouter.patch("/notes/:noteId", updateNote);
+notesRouter.get("/", getAllNotes);
+notesRouter.get("/:noteId", getNoteById);
+notesRouter.post("/", createNote);
+notesRouter.delete("/:noteId", deleteNote);
+notesRouter.patch("/:noteId", updateNote);
 
-
-export const notesRouterExport = notesRouter;
+export default notesRouter;
--- src/server.js
+++ src/server.js
@@ -6,8 +6,7 @@ import cors from 'cors';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import { notesRouterExport } from "./routes/notesRoutes.js";
-
+import notesRouter from "./routes/notesRoutes.js";
 
 await connectMongoDB();
 
@@ -16,7 +15,7 @@ const app = express();
 app.use(logger);
 app.use(express.json());
 app.use(cors());
-app.use("/", notesRouterExport);
+app.use("/notes", notesRouter);
 app.use(notFoundHandler); 
 app.use(errorHandler);
 
